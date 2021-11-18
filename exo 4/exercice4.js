f68404c389bf800fad9a22ad082ce7d8a93b9b09
Noah2211 <noah.decroix3@gmail.com>
2021-11-18 13:36:14 +0100

    exercices done

--- exo 4/exercice4.js
+++ exo 4/exercice4.js
@@ -1,9 +1,11 @@
 /* Ecrire ci dessous une fonction permettant d'ajouter du texte dans le div appelé container */
 
-
+function textDiv () {
+    document.getElementById("container").innerHTML = "Mon super texte"
+}
 
 /* Appeler la fonction */
-
+textDiv();
 
 
 
